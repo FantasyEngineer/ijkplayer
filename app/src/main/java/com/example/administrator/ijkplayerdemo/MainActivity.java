f98106ea6b7f3f398e720a16ef9f119df2813ebd
houjiguo <jiguo.hou@dragonsoftbravo.com>
2018-10-23 14:54:29 +0800

    Revert "测试dev分支"
    This reverts commit 7d6faa71c85bce5d4b397df1856c37124c34475e.

--- app/src/main/java/com/example/administrator/ijkplayerdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/ijkplayerdemo/MainActivity.java
@@ -131,7 +131,6 @@ public class MainActivity extends AppCompatActivity {
                                 .getColumnIndexOrThrow(MediaStore.Video.Media.SIZE));
             }
             cursor.close();
-            /*测试dev*/
         }
         return list;
     }
